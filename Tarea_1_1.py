1dda85d18b7a9fd23ff9b4ee449e05b839abc92e
DanielTecchi <60267850+DanielTecchi@users.noreply.github.com>
2020-02-12 17:22:13 -0600

    Add files via upload

---
+++ Tarea_1_1.py
@@ -0,0 +1,18 @@
+class restaurant():
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name=restaurant_name
+        self.cuisine_type=cuisine_type
+        self.number_served=0
+    def describe_res(self):
+        print("El restaurante " + self.restaurant_name.title() + " trabaja comida " + self.cuisine_type.title())
+    def open_restaurant(self):
+        print( self.restaurant_name.title()  +" abre de lunes a sabado de 9:30am a 23:00hrs")
+    def customers(self):
+        print("Contamos con "+ str(self.number_served)+"clientes en estancia.")
+        
+
+my_restaurant=restaurant("the cruw", "americana")
+my_restaurant.describe_res()
+my_restaurant.open_restaurant()
+my_restaurant.number_served=32
+my_restaurant.customers()
